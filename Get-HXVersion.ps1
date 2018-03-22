18d30850bcc3631d897e0c16c970c692fb14240e
Daniel Ferreira <daniel@ferreira.fm>
2018-03-22 15:50:35 +0100

    adding version cmdlet

--- Get-HXVersion.ps1
+++ Get-HXVersion.ps1
@@ -14,7 +14,26 @@ function Get-HXVersion {
 
     begin { }
     process {
-        
+
+        # Uri filtering:
+        if ($Uri -match '\d$') { 
+            $Endpoint = $Uri+'/hx/api/v3/version'
+            Write-Verbose "Endpoint: $Endpoint"
+        }
+        else { $Endpoint = $Uri }
+
+        # Request:
+        $WebRequest = Invoke-WebRequest -Uri $Endpoint -WebSession $WebSession -Method Get 
+
+        # Return the object:
+        $out = New-Object System.Object
+        $out | Add-Member -Type NoteProperty -Name Uri -Value $Uri
+        $out | Add-Member -Type NoteProperty -Name Endpoint -Value $Endpoint
+        $out | Add-Member -Type NoteProperty -Name WebSession -Value $WebSession
+        $out | Add-Member -Type NoteProperty -Name TokenSession -Value $TokenSession
+        $out | Add-Member -Type NoteProperty -Name RequestStatusCode -Value $WebRequest.StatusCode
+        $out | Add-Member -Type NoteProperty -Name RequestContent -Value $WebRequest.Content
+        $out
     }
     end { }
 }
