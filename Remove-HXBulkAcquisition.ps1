e47a17a40059e2ba570370a527e817dc71801a23
Daniel Ferreira <daniel@ferreira.fm>
2018-05-08 10:20:18 +0200

    adding cmdlet to delete bulk acquisitions

---
+++ Remove-HXBulkAcquisition.ps1
@@ -0,0 +1,42 @@
+function Remove-HXBulkAcquisition {
+    [CmdletBinding()]
+    [OutputType([psobject])]
+    param(
+        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [string] $Uri,
+
+        [Parameter(Mandatory=$false, ValueFromPipelineByPropertyName=$true)]
+        [Microsoft.PowerShell.Commands.WebRequestSession] $WebSession,
+
+        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [string] $TokenSession, 
+
+        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [Alias("bulkacquisition_id")]
+        [int] $BulkAcquisitionId,
+
+        [Parameter(Mandatory=$false)]
+        [switch] $Passthru,
+
+        [Parameter(Mandatory=$false)]
+        [switch] $Raw
+    )
+
+    begin { }
+    process {
+
+        # Uri filtering:
+        if ($Uri -match '\d$') { $Endpoint = $Uri+"/hx/api/v3/acqs/bulk/$BulkAcquisitionId" }
+        elseif ($Uri -match '\d/$') { $Endpoint = $Uri+"hx/api/v3/acqs/bulk/$BulkAcquisitionId" }
+        else { $Endpoint = $Uri + "/?" }
+
+        # Header:
+        $headers = @{ "Accept" = "application/json" }
+        if (-not($WebSession) -and ($TokenSession)) { $headers += @{ "X-FeApi-Token" = $TokenSession } }
+
+        # Request:
+        $WebRequest = Invoke-WebRequest -Uri $Endpoint -WebSession $WebSession -Method Delete -Headers $headers -SkipCertificateCheck
+        
+    }
+    end { }
+}
