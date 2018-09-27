f6c2763623d0482bfb6467d9a7b0d3e3973b2365
Daniel Ferreira <daniel@ferreira.fm>
2018-09-27 10:21:08 +0200

    improved hxsesssion cmdlet

--- New-HXSession.ps1
+++ New-HXSession.ps1
@@ -28,18 +28,11 @@ function New-HXSession {
         # Make the request to the controller:
         $WebRequest = Invoke-WebRequest -Uri $Endpoint -Method Get -SessionVariable LoginSession -ErrorAction Stop -Headers $headers -SkipCertificateCheck 
 
-        $TokenSession = $WebRequest.Headers.'X-FeApi-Token' | out-string -NoNewline
-        # -NoNewLine was introduced in PS6.0, so use below to enable Windows PowerShell backwards compatibility:
-        #$TokenSession = $TokenSession -replace "`t|`n|`r","" # bugfix: 'out-string' introduce a new-line at the end of the string. This hack will remove it. 
-        
-        if ($TokenSession -eq $null) { throw "Login token not observed in the authentication response." }
-        
         # Return the object:
         $out = New-Object System.Object
         $out | Add-Member -Type NoteProperty -Name Uri -Value $Uri
         $out | Add-Member -Type NoteProperty -Name Endpoint -Value $Endpoint
         $out | Add-Member -Type NoteProperty -Name WebSession -Value $LoginSession
-        $out | Add-Member -Type NoteProperty -Name TokenSession -Value $TokenSession
         $out
     }
     end { }
