5640e385f4f8db3ad72bb9b9ace2ec65f58fa834
Daniel Ferreira <daniel@ferreira.fm>
2018-10-07 01:17:30 +0200

    hxhost cmdlet

---
+++ Find-HXHost.ps1
@@ -0,0 +1,93 @@
+function Find-HXHost {
+    [CmdletBinding()]
+    [OutputType([psobject])]
+    param(    
+        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true)]
+        [string] $Uri,
+
+        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true)]
+        [Microsoft.PowerShell.Commands.WebRequestSession] $WebSession,
+
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true)]
+        [string] $Search,
+
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true)]
+        [string] $Offset,
+
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true)]
+        [string] $Limit,
+
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true)]
+        [string] $Sort,
+
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true)]
+        [string] $Filter,
+
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true)]
+        [switch] $HasAlerts,
+
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true)]
+        [switch] $HasActiveThreats,
+        
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true)]
+        [switch] $HasExecutionAlerts,
+        
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true)]
+        [switch] $HasExploitAlerts,
+
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true)]
+        [switch] $HasExploitBlocks,
+
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true)]
+        [switch] $HasMalwareAlerts,
+
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true)]
+        [switch] $HasMalwareCleaned,
+
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true)]
+        [switch] $HasMalwareQuarantined,
+
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true)]
+        [switch] $HasPresenceAlerts
+    )
+
+    begin { }
+    process {
+        # Uri filtering:
+        if ($Uri -match '\d$') { $Endpoint = $Uri + "/hx/api/v3/hosts/?" }
+        elseif ($Uri -match '\d/$') { $Endpoint = $Uri + "hx/api/v3/hosts/?" }
+        else { $Endpoint = $Uri + "/?" }
+
+        # Header:
+        $headers = @{ "Accept" = "application/json" }
+
+        if ($Search) { $Endpoint = $Endpoint + "&search=" + $Search }
+        if ($Offset) { $Endpoint = $Endpoint + "&offset=" + $Offset }
+        if ($Limit) { $Endpoint = $Endpoint + "&limit=" + $Limit }
+        if ($Sort) { $Endpoint = $Endpoint + "&sort=" + $Sort }
+        if ($HasAlerts) { $Endpoint = $Endpoint + "&has_alerts=1" }
+        if ($HasActiveThreats) { $Endpoint = $Endpoint + "&has_active_threats=1" }
+        if ($HasExecutionAlerts) { $Endpoint = $Endpoint + "&has_execution_alerts=1" }
+        if ($HasExploitAlerts) { $Endpoint = $Endpoint + "&has_exploit_alerts=1" }
+        if ($HasExploitBlocks) { $Endpoint = $Endpoint + "&has_exploit_blocks=1" }
+        if ($HasMalwareAlerts) { $Endpoint = $Endpoint + "&has_malware_alerts=1" }
+        if ($HasMalwareCleaned) { $Endpoint = $Endpoint + "&has_malware_cleaned=1" }
+        if ($HasMalwareQuarantined) { $Endpoint = $Endpoint + "&has_malware_quarantined=1" }
+        if ($HasPresenceAlerts) { $Endpoint = $Endpoint + "&has_presence_alerts=1" }
+        if ($Filter) { $Endpoint = $Endpoint + "&" + $Filter }
+
+        # Request:
+        $WebRequest = Invoke-WebRequest -Uri $Endpoint -WebSession $WebSession -Method Get -Headers $headers -SkipCertificateCheck
+        $WebRequestContent = $WebRequest.Content | ConvertFrom-Json
+
+        # Return the object:
+        $WebRequestContent.data.entries.foreach( {
+            # This will add the previous objects of the pipeline to the output, as well as will produce the output of the function at the same time:
+                $_ | Select-Object @{name = 'host_id'; e = {$_._id}}, `
+                    @{name = 'Uri'; e = {$Uri}}, `
+                    @{name = 'WebSession'; e = {$WebSession}}, `
+                    * | Select-Object -ExcludeProperty _id
+            })
+    }
+    end { }
+}
